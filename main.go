47047d33b659b503e12c6174a35c183012c0e9be
theo desktop <theo.bm.pro@gmail.com>
2023-12-10 15:08:27 +0100

    day 10 p1 & p2

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 		Day2(),
 		Day3(),
 		Day4(),
-		{0, 0}, //Day5(),
+		{-1, -1}, //Day5(),
 		Day6(),
 		Day7(),
 		Day8(),
@@ -33,18 +33,22 @@ func testResults(result [][2]int) {
 		{253866470, 254494947},
 		{17873, 15746133679061},
 		{1987402313, 900},
-		{-1, -1},
+		{6886, 371},
 	}
 
 	for i := 0; i < len(expectedResults); i++ {
 		if expectedResults[i][0] == result[i][0] {
 			fmt.Println("\033[32mDay: ", i+1, ", Part: 1, PASS result :", result[i][0], " matching expected result ", expectedResults[i][0], "\033[0m")
+		} else if result[i][0] == -1 {
+			fmt.Println("Day: ", i+1, ", Part: 1, SKIPED expected result ", expectedResults[i][0])
 		} else {
 			fmt.Println("\033[31mDay: ", i+1, ", Part: 1, FAIL result :", result[i][0], " not matching expected result ", expectedResults[i][0], "\033[0m")
 		}
 
 		if expectedResults[i][1] == result[i][1] {
 			fmt.Println("\033[32mDay: ", i+1, ", Part: 2, PASS result :", result[i][1], " matching expected result ", expectedResults[i][1], "\033[0m")
+		} else if result[i][1] == -1 {
+			fmt.Println("Day: ", i+1, ", Part: 2, SKIPED expected result ", expectedResults[i][1])
 		} else {
 			fmt.Println("\033[31mDay: ", i+1, ", Part: 2, FAIL result :", result[i][1], " not matching expected result ", expectedResults[i][1], "\033[0m")
 		}
