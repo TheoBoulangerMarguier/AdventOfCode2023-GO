32704ecd2c8a8976c4ce96e301019da29c8add43
theo desktop <theo.bm.pro@gmail.com>
2023-12-22 17:35:22 +0100

    templates for d18 => d24

---
+++ Day23/day23.go
@@ -0,0 +1,18 @@
+/* */
+
+package Day23
+
+func Day23() [2]int {
+	return [2]int{
+		d23p1(),
+		d23p2(),
+	}
+}
+
+func d23p1() int {
+	return 0
+}
+
+func d23p2() int {
+	return 0
+}
---
+++ Day24/day24.go
@@ -0,0 +1,18 @@
+/* */
+
+package Day24
+
+func Day24() [2]int {
+	return [2]int{
+		d24p1(),
+		d24p2(),
+	}
+}
+
+func d24p1() int {
+	return 0
+}
+
+func d24p2() int {
+	return 0
+}
